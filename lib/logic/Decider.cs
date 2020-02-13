d48bd4022f60458c984aa015ea3641536261ef25
CaKellum <cakellum@bsu.edu>
2020-02-13 17:20:23 -0500

    populating things

--- lib/logic/Decider.cs
+++ lib/logic/Decider.cs
@@ -1,5 +1,7 @@
 public class Decider{
+
+    
     public string getCurrentLead(){
-        
+
     }
 }
---
+++ lib/objects/Horse.cs
@@ -0,0 +1,5 @@
+public class Horse{
+    private string name;
+    private int topODD;
+    private int bottomODD;
+}
