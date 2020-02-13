482710a5d00228dba7c27007038b496f5840ba4f
CaKellum <cakellum@bsu.edu>
2020-02-13 17:36:32 -0500

    save stuff

---
+++ lib/AtomicDowns.java
@@ -0,0 +1,5 @@
+public class AtomicDowns{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ lib/logic/Decider.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Decider{
+
+    private ArrayList<Horse> horses;
+
+    Decider(ArrayList<Horse> horses){
+        this.horses = horses;
+    }
+}
